2df71a6a7c4c2c9b02032e7ccfbba9fa47d2fdb6
yambai <magadiev.123789@gmail.com>
2024-02-26 05:55:25 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,110 @@
+package com.example.myapplication;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if  (!isConnected()){
+            Toast.makeText(SecondActivity.this, "Отсутствует интернет", Toast.LENGTH_SHORT).show();
+        }
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.pass_word);
+        MaterialButton login_btn = (MaterialButton) findViewById(R.id.login_btn);
+        MaterialButton forgot_btn = (MaterialButton) findViewById(R.id.forgotPassword);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    Toast.makeText(SecondActivity.this, "URA", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(SecondActivity.this, "FUUUUU", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        forgot_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isConnected()){
+                    String url = "https://isu.uust.ru/re_password/";
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+// Устанавливаем пакет для Google Chrome
+                    browserIntent.setPackage("com.android.chrome");
+
+                    try {
+                        startActivity(browserIntent);
+                    } catch (ActivityNotFoundException ex) {
+                        // Chrome браузер не установлен
+                        browserIntent.setPackage(null);
+                        startActivity(browserIntent);
+                    }
+
+                }else {
+                    Toast.makeText(SecondActivity.this, "Отсутствует интернет", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+    }
+
+    boolean isConnected(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if(networkInfo!=null){
+            return networkInfo.isConnected();
+        }else return false;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
